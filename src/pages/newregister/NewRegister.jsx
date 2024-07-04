9b6462dcc7c84563a98c6d43f8a8eb5e4deabe96
arik <raiarik97@gmail.com>
2024-07-04 21:00:31 +0545

    Registration Changed As Needed

--- src/pages/newregister/NewRegister.jsx
+++ src/pages/newregister/NewRegister.jsx
@@ -2,43 +2,63 @@ import React from "react";
 
 const NewRegister = () => {
   return (
-    <div className="container m-5">
-      <h3 className="text-success">
-        Create your <span className="text-dark">Account!</span>{" "}
-      </h3>
-      <form className="w-50 shadow p-4 rounded-4">
-        <label htmlFor="firstname">Firstname</label>
-        <input
-          className="form-control"
-          type="text"
-          placeholder="Enter your firstname"
-        />
-        <label className="mt-2" htmlFor="lastname">
-          Lastname
-        </label>
-        <input
-          className="form-control"
-          type="text"
-          placeholder="Enter your lastname"
-        />
-        <label className="mt-2" htmlFor="email">
-          Email
-        </label>
-        <input
-          className="form-control"
-          type="email"
-          placeholder="Enter your email"
-        />
-        <label className="mt-2" htmlFor="password">
-          Password
-        </label>
-        <input
-          className="form-control"
-          type="password"
-          placeholder="Enter your password"
-        />
-        <button className="btn btn-dark mt-3 w-100">Sign up</button>
-      </form>
+    <div className="container-fluid" style={{ height: "100vh" }}>
+      <div className="row h-100">
+        <div className="col-6 d-flex align-items-center justify-content-center">
+          <img src="./assets/images/banner2.png" alt="Logo" />
+        </div>
+        <div className="col-6 d-flex flex-column align-items-center justify-content-center">
+          <h1 className="">Register</h1>
+          <form className="w-75">
+            <label htmlFor="firstname">Firstname</label>
+            <input
+              className="form-control"
+              type="text"
+              id="firstname"
+              placeholder="Enter your firstname"
+            />
+            <label className="mt-2" htmlFor="lastname">
+              Lastname
+            </label>
+            <input
+              className="form-control"
+              type="text"
+              id="lastname"
+              placeholder="Enter your lastname"
+            />
+            <label className="mt-2" htmlFor="email">
+              Email
+            </label>
+            <input
+              className="form-control"
+              type="email"
+              id="email"
+              placeholder="Enter your email"
+            />
+            <label className="mt-2" htmlFor="password">
+              Password
+            </label>
+            <input
+              className="form-control"
+              type="password"
+              id="password"
+              placeholder="Enter your password"
+            />
+            <div className="form-check mt-3">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                id="autoSizingCheck"
+              />
+              <label className="form-check-label" htmlFor="autoSizingCheck">
+                I agree to all statements included in
+                <span className="text-info"> terms of services</span>
+              </label>
+            </div>
+            <button className="btn btn-dark mt-3 w-100">Sign up</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };

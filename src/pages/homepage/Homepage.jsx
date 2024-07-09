ff2fdb6949d7453711d02e04982a8fbdb1447bf8
arik <raiarik97@gmail.com>
2024-07-09 14:58:25 +0545

    Pushing some changes

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,9 +1,17 @@
 // First: Import - React
 import React, { useEffect } from "react";
 import { fetchUsers } from "../../api/Api";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "../../redux_storage/userSlice";
 
 // Second: Make a function (Filename)
 const Homepage = () => {
+  //Make a dispatch
+  const dispatch = useDispatch();
+
+  // Selector (Select from storage)
+  const users = useSelector((state) => state.users.users);
+
   // Automatic user fetch ( useEffect )
   // [] is dependency list, when should it run
   useEffect(() => {
@@ -11,13 +19,46 @@ const Homepage = () => {
     fetchUsers()
       .then((res) => {
         console.log(res.data);
+
+        // Adding user to redux
+
+        dispatch(addUser(res.data));
       })
       .catch((error) => {
         console.log(error);
       });
   }, []);
 
-  return <div>Its Homepage!</div>;
+  return (
+    <>
+      {/* table */}
+      <table className="table">
+        <thead className="table-dark">
+          <tr>
+            <th>Id</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>123</td>
+            <td>Sujan</td>
+            <td>Baidhya</td>
+            <td>ILove69@gmail.com</td>
+            <td>
+              <di className="btn-group">
+                <button className="btn btn-success">Edit</button>
+                <button className="btn btn-danger">Delete</button>
+              </di>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
 };
 
 // Export
